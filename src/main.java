1ce8f6a746e1d05a24bb89ab799edaf827634969
Dawid Czarneta <dawid.code+github@gmail.com>
2015-08-18 10:12:19 +0200

    test pwershella

--- src/main.java
+++ src/main.java
@@ -2,8 +2,10 @@
  * Created by Dawid on 18.08.2015.
  */
 import java.util.Scanner;
+
 public class main {
     public static void main(String[] args){
         System.out.println("Calculate age in seconds");
+        System.out.println("Test pwershella");
     }
 }
