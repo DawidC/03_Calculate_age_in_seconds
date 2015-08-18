fb4523a2393ff6988e1777ff484b11142ccc5345
Dawid Czarneta <dawid.code+github@gmail.com>
2015-08-18 10:09:43 +0200

    Test 2

--- src/main.java
+++ src/main.java
@@ -1,7 +1,7 @@
 /**
  * Created by Dawid on 18.08.2015.
  */
-
+import java.util.Scanner;
 public class main {
     public static void main(String[] args){
         System.out.println("Calculate age in seconds");
